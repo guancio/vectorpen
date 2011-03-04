5cdc173d5b2a6bb7abfc5c2a85eaae4ac225da49
Roberto Guanciale <guancio@gmail.com>
2011-03-04 11:42:52 +0100

    Command line options to process a file or the std out and in

--- src-cli/com/vectorpen/cli/Main.java
+++ src-cli/com/vectorpen/cli/Main.java
@@ -3,6 +3,7 @@ package com.vectorpen.cli;
 import java.io.File;
 
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
 
@@ -10,26 +11,122 @@ import com.vectorpen.core.FileInput;
 import com.vectorpen.core.VectorFile;
 import com.vectorpen.core.Path;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+
+import org.apache.commons.cli.ParseException;
+
 public class Main {
     static public void main(String args[]) {
-	try {
-	    String fileName = "samples/top/aiptek-mynote.top";
-	    VectorFile file = FileInput.readFile(new File(fileName));
-	    System.out.println("File successfully opened");
+	Options options = new Options();
+	options.addOption(
+			  OptionBuilder.withArgName( "help" )
+			  .withDescription(  "this help page" )
+			  .withLongOpt("help") 
+			  .create( "h" )
+			  );
+	options.addOption(
+			  OptionBuilder.withArgName( "input" )
+			  .hasArg()
+			  .withDescription(  "use given file/path for input" )
+			  .withLongOpt("input") 
+			  .create( "i" )
+			  );
+	options.addOption(
+			  OptionBuilder.withArgName( "format" )
+			  .hasArg()
+			  .withDescription(  "process stdin using the given format. e.g. top" )
+			  .withLongOpt("format") 
+			  .create( "f" )
+			  );
+	options.addOption(
+			  OptionBuilder.withArgName( "title" )
+			  .hasArg()
+			  .withDescription(  "process stdin using the given title" )
+			  .withLongOpt("title") 
+			  .create( "t" )
+			  );
+	options.addOption(
+			  OptionBuilder.withArgName( "output" )
+			  .hasArg()
+			  .withDescription(  "use given file/path for output" )
+			  .withLongOpt("output") 
+			  .create( "o" )
+			  );
 
-	    FileOutputStream fileOutputStream = new FileOutputStream(fileName+".svg");
-	    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream , "ISO-8859-1"));
+	CommandLineParser parser = new GnuParser();
+	CommandLine line = null;
+	try {
+	    line = parser.parse( options, args );
+	}
+	catch (ParseException ex) {
+	    HelpFormatter formatter = new HelpFormatter();
+	    formatter.printHelp( "vectorpen", options );
+	    return;
+	}
+
+	String input = null;
+	String format = null;
+	String title = null;
+	String output = null;
+
+	if (line.hasOption("h")) {
+	    HelpFormatter formatter = new HelpFormatter();
+	    formatter.printHelp( "vectorpen", options, true);
+	    return;
+	}
+
+	input = line.getOptionValue("i");
+	if (input != null)
+	    input = input.trim();
+	format = line.getOptionValue("f");
+	if (format != null)
+	    format = format.trim();
+	title = line.getOptionValue("t");
+	if (title != null)
+	    title = title.trim();
+	output = line.getOptionValue("o");
+	if (output != null)
+	    output = title.trim();
+
+	// System.out.println(input);
+	// System.out.println(title);
+	// System.out.println(format);
+
+	
+	try {
+	    VectorFile file = null;
+
+	    if (input != null) {
+		file = FileInput.readFile(new File(input));
+	    }
+	    else {
+		file = FileInput.readStream(System.in, title, format);
+	    }
+
+	    OutputStream out = System.out;
+	    if (output != null)
+		out = new FileOutputStream(output);
+	    
+	    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out , "ISO-8859-1"));
 	    writer.write(file.getSVGRepresentation());
 	    writer.close();
 	    writer = null;
 	    
-	    fileOutputStream.close();
+	    out.flush();
 	    
 	    System.gc();
 	}
 	catch (java.io.IOException ex) {
+	    ex.printStackTrace();
 	}
 	catch (java.util.zip.DataFormatException ex) {
+	    ex.printStackTrace();
 	}
     }
 }
--- src-core/com/vectorpen/core/FileInput.java
+++ src-core/com/vectorpen/core/FileInput.java
@@ -28,57 +28,48 @@ package com.vectorpen.core;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.zip.DataFormatException;
 
 public final class FileInput
 {
-	public static VectorFile readFile(File file)
+    public static VectorFile readStream(InputStream stream, String title, String fileExtension)
+	throws IOException, DataFormatException {
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	byte[] buffer = new byte[1024];
+	int n = 0;
+	while ((n = stream.read(buffer)) != -1) {
+	    out.write(buffer, 0, n);
+	}
+	byte data[] = out.toByteArray();
+	if (fileExtension.compareToIgnoreCase(".DHW") == 0 && DHWModule.checkDHWFileHeader(data)) {
+	    return DHWModule.readDHWFile(data, title);
+	}
+	else if (fileExtension.compareToIgnoreCase(".TOP") == 0 && TOPModule.checkTOPFileHeader(data)) {
+	    return TOPModule.readTOPFile(data, title);
+	}
+	else if (fileExtension.compareToIgnoreCase(".DNT") == 0 && DNTModule.checkDNTFileHeader(data)) {
+	    return DNTModule.readDNTFile(data, title);
+	}
+	else {
+	    throw new DataFormatException(ExceptionCodes.FILE_EXTENSION);
+	}
+    }
+    
+    public static VectorFile readFile(File file)
 	throws IOException, DataFormatException
 	{
-		try
-		{
-			FileInputStream fileInputStream = new FileInputStream(file);
-			byte data[] = new byte[(int)file.length()];
-
-			fileInputStream.read(data);
-			fileInputStream.close();
-
-			String fileExtension = getExtension(file.getName());
-			String title = getTitle(file.getName());
-
-			if (fileExtension.compareToIgnoreCase(".DHW") == 0 && DHWModule.checkDHWFileHeader(data))
-			{
-				return DHWModule.readDHWFile(data, title);
-			}
-			else if (fileExtension.compareToIgnoreCase(".TOP") == 0 && TOPModule.checkTOPFileHeader(data))
-			{
-				return TOPModule.readTOPFile(data, title);
-			}
-			else if (fileExtension.compareToIgnoreCase(".DNT") == 0 && DNTModule.checkDNTFileHeader(data))
-			{
-				return DNTModule.readDNTFile(data, title);
-			}
-			else
-			{
-				throw new DataFormatException(ExceptionCodes.FILE_EXTENSION);
-			}
-		}
-		catch (Exception exception)
-		{
-			if (exception.getClass() == DataFormatException.class)
-			{
-				throw new DataFormatException(exception.getMessage());
-			}
-			else
-			{
-				throw new IOException(ExceptionCodes.FILE_READ);
-			}
-		}
+	    FileInputStream fileInputStream = new FileInputStream(file);
+	    
+	    String fileExtension = getExtension(file.getName());
+	    String title = getTitle(file.getName());
+	    return FileInput.readStream(fileInputStream, title, fileExtension);
 	}
 
 	private static String getExtension(String fileName)
-	throws Exception
+	//	throws Exception
 	{
 		int dotPosition = fileName.lastIndexOf(".");
 
@@ -86,7 +77,7 @@ public final class FileInput
 	}
 
 	private static String getTitle(String fileName)
-	throws Exception
+	//throws Exception
 	{
 		int dotPosition = fileName.lastIndexOf(".");
 
