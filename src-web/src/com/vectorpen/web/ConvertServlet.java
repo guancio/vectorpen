5cf945716a3d74afb8a810d2caf3028a45ac94c6
Roberto Guanciale <guancio@gmail.com>
2011-03-08 15:07:13 +0100

    Sample Web app conversion of pdf

--- src-web/src/com/vectorpen/web/ConvertServlet.java
+++ src-web/src/com/vectorpen/web/ConvertServlet.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+
+import java.util.List;
+import java.util.Vector;
 import java.util.logging.Logger;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
 
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
@@ -16,6 +21,8 @@ import java.io.BufferedWriter;
 
 import com.vectorpen.core.FileInput;
 import com.vectorpen.core.VectorFile;
+import com.vectorpen.core.PDFModule;
+import com.vectorpen.core.DocInfoDict;
 
 public class ConvertServlet extends HttpServlet {
     private static final Logger log =
@@ -33,15 +40,23 @@ public class ConvertServlet extends HttpServlet {
 
 	try {
 	    ServletFileUpload upload = new ServletFileUpload();
-	    resp.setContentType("text/svg");
 
-	    FileItemIterator iterator = upload.getItemIterator(req);
-	    while (iterator.hasNext()) {
-		FileItemStream item = iterator.next();
+	    VectorFile vec = null;
+	    String outFormat = "SVG";
+
+	    FileItemIterator iter = upload.getItemIterator(req);
+	    while (iter.hasNext()) {
+		FileItemStream item = iter.next();
 		InputStream stream = item.openStream();
 
 		if (item.isFormField()) {
+		    String value = Streams.asString(stream);
 		    log.warning("Got a form field: " + item.getFieldName());
+		    if ("SVG".equals(item.getFieldName()) && "SVG".equalsIgnoreCase(value))
+			outFormat = "SVG";
+		    if ("PDF".equals(item.getFieldName()) && "PDF".equalsIgnoreCase(value))
+			outFormat = "PDF";
+			
 		} else {
 		    log.warning("Got an uploaded file: " + item.getFieldName() +
 				", name = " + item.getName());
@@ -56,14 +71,33 @@ public class ConvertServlet extends HttpServlet {
 		    // will probably want to do something more interesting (for
 		    // example, wrap them in a Blob and commit them to the
 		    // datastore).
-		    VectorFile vec = FileInput.readStream(stream, title, format);
-		    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream() , "ISO-8859-1"));
-		    writer.write(vec.getSVGRepresentation());
-		    writer.flush();
-
-		    log.warning("Conversion completed");
+		    vec = FileInput.readStream(stream, title, format);
 		}
 	    }
+
+	    if ("SVG".equalsIgnoreCase(outFormat)) {
+		resp.setContentType("text/xml" );
+		resp.setHeader( "Content-Disposition", "attachment; filename=\"" + vec.getTitle() + ".svg\"" );
+
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream() , "ISO-8859-1"));
+		writer.write(vec.getSVGRepresentation());
+		writer.flush();
+	    }
+	    else if ("PDF".equalsIgnoreCase(outFormat)) {
+		resp.setContentType("application/pdf" );
+		resp.setHeader( "Content-Disposition", "attachment; filename=\"" + vec.getTitle() + ".pdf\"" );
+
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream() , "ISO-8859-1"));
+		List<String> keys = new Vector<String>();
+		keys.add("ProducedWithVectorPen");
+		List<VectorFile> vecs = new Vector<VectorFile>();
+		vecs.add(vec);
+		DocInfoDict docInfoDict = new DocInfoDict(vec.getTitle(), "Author", "Subject", keys);
+		writer.write(PDFModule.getPDFData(vecs, docInfoDict));
+		writer.flush();
+	    }
+	    
+	    log.warning("Conversion completed");
 	} catch (Exception ex) {
 	    throw new ServletException(ex);
 	}
